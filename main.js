f5f5117c6a07ecfc0724c5cd62e6a5a488ac0110
emackinnon1 <emackinnon100390@gmail.com>
2020-02-28 12:18:35 -0700

    MakeToDoList creates a new ToDo but does not clear old currentTask Array or nodeList

--- main.js
+++ main.js
@@ -13,38 +13,37 @@
 // DOM nodes
 var addTaskBtn = document.querySelector('.add-task-btn');
 var newTaskInput = document.querySelector('.new-task-input');
-var taskTitle = document.querySelector('.task-title');
+// var toDoTitle = document.querySelector('.todo-title');
 var currentTasksDisplay = document.querySelector('.current-tasks-display');
+var makeToDoBtn = document.querySelector('.new-todo-btn');
 
 // global variables
 var currentTasks = [];
+var listOfToDoLists = [];
 
 
 addTaskBtn.addEventListener('click', makeNewCurrentTask);
 currentTasksDisplay.addEventListener('click', deleteCurrentTask);
+makeToDoBtn.addEventListener('click', makeToDoList);
 
+// displays prompt if there are no Todo lists in localStorage
 window.onload = function() {
   if (!localStorage.getItem('toDos')) {
     document.querySelector('.prompt').classList.remove('hidden');
   }
 }
 
+// instantiates new task according to user input
 function makeNewCurrentTask() {
-  // var currentTasks = [];
   if (newTaskInput.value === '') {
     return;
   }
   var task = new Task(newTaskInput.value);
-
   currentTasks.push(task);
   displayCurrentTasks(currentTasks);
-
-  var currentTaskNodeList = document.querySelectorAll('.current-task');
-
-  console.log("current task node list", currentTaskNodeList);
-  console.log("current task array", currentTasks);
 }
 
+// displays task objects held in currentTasks array on DOM
 function displayCurrentTasks(potentialTaskList) {
   currentTasksDisplay.innerHTML = '';
   for (var i = 0; i < potentialTaskList.length; i++) {
@@ -55,22 +54,25 @@ function displayCurrentTasks(potentialTaskList) {
   newTaskInput.value = '';
 }
 
+// deletes selected task from DOM and from currentTasks array
 function deleteCurrentTask(event) {
   if (event.target.matches('.delete-current-task')) {
     console.log(event.target.closest('li').classList[1].slice(4));
     var index = event.target.closest('li').classList[1].slice(4);
     currentTasks.splice(index, 1);
     event.target.closest('li').remove();
-
-    var currentTaskNodeList = document.querySelectorAll('.current-task');
-
-    console.log("current task node list", currentTaskNodeList);
-    console.log("current task array", currentTasks);
   }
 }
 
+// creates to do list
+function makeToDoList() {
+  var toDo = new ToDoList(document.querySelector('.todo-title'), currentTasks);
+  listOfToDoLists.push(toDo);
+  console.log(listOfToDoLists)
+}
+
 function displayToDos() {
-  var toDoListArray = JSON.parse(localStorage.getItem('toDos'));
-  for (var i = 0; i < toDoListArray.length; i++) {
+  var toDoListStorageArray = JSON.parse(localStorage.getItem('toDos'));
+  for (var i = 0; i < toDoListStorageArray.length; i++) {
   }
 }
--- todo-list.js
+++ todo-list.js
@@ -2,7 +2,7 @@ class ToDoList {
   constructor(title, tasks) {
     this.title = title;
     this.tasks = tasks;
-    this.timeStamp = date.now();
+    this.timeStamp = Date.now();
     this.urgent = false;
     this.delete = false;
   }
